584f8abb5b0438a0c35145985bdfb9fce3e2fbc2
praaannnn <fransxianiparz@gmail.com>
2022-09-08 20:51:08 +0700

    update

--- soal2.js
+++ soal2.js
@@ -15,8 +15,8 @@
 
 
     class Shark extends Animal{
-        constructor(name, age, legs, species, status){
-            super(name, age, legs, species, status)
+        constructor(name, age, status){
+            super(name, age, 0, "shark", status)
         }
         print() {
             return `nama ${this.name}, umur ${this.age}, kaki ${this.legs}, spesies ${this.species}, status ${this.status}\n`
@@ -24,8 +24,8 @@
     }
 
     class Cat extends Animal{
-        constructor(name, age, legs, species, status){
-            super(name, age, legs, species, status)
+        constructor(name, age, status){
+            super(name, age, 4, "cat", status)
         }
         print() {
             return `nama ${this.name}, umur ${this.age}, kaki ${this.legs}, spesies ${this.species}, status ${this.status}\n`
@@ -34,24 +34,19 @@
     }
 
     class Dog extends Animal{
-        greatMaster(){
-            return `hello,my name is ${this.name} `;
+        greetMaster(){
+            return `Hello ${this.master}`;
         }
-        constructor(name, age, legs, species, status){
-            super(name, age, legs, species, status)
+        constructor(name, age, status, master){
+            super(name, age, 4, "dog", status)
+            this.master = master
         }
         print() {
             return `nama ${this.name}, umur ${this.age}, kaki ${this.legs}, spesies ${this.species}, status ${this.status}\n`
         }
-
     }
-    const hiu = new Shark('Hiu', 10, 0, 'Shark', 'Berenang')
-    const cat = new Cat('cat', 5, 4, 'Cat', 'darat')
-    const dog = new Dog('Master', 8, 4, 'Dog', 'langka')
-    console.log(hiu.introduce())
+    const hiu = new Shark('Hiu', 10, 'Berenang')
+    const cat = new Cat('cat', 5, 'Berjalan')
+    const dog = new Dog('Dog', 8, 'Berlari', 'Master')
+
     console.log(hiu.print())
-    console.log(cat.introduce())
-    console.log(cat.print())
-    console.log(dog.greatMaster())
-    console.log(dog.print())
-    
